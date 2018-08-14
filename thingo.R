b4ebb7895101cf267d209631023ee1100efcf318
David Lawrence Miller <github@ninepointeightone.net>
2018-08-14 20:59:28 +0100

    ...

--- thingo.R
+++ thingo.R
@@ -43,6 +43,7 @@ fuzzy_dog <- system2("/usr/local/bin/twurl",
                          fn,
                          " -F media -X POST"))
 fuzzy_dog <- sub(".*\"media_id\":(\\d+).*", "\\1", fuzzy_dog)
+cat(fuzzy_dog,"\n")
 fuzzy_dog2 <- system2("/usr/local/bin/twurl",
                       paste0("\"/1.1/statuses/update.json\" -d \"media_ids=",
                              fuzzy_dog,
