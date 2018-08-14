11901101b09d6ebac26d119b14681ad2cd57a56d
David Lawrence Miller <github@ninepointeightone.net>
2018-08-14 21:00:28 +0100

    blah

--- thingo.R
+++ thingo.R
@@ -41,7 +41,7 @@ fn <- generate_map(dat, xy)
 fuzzy_dog <- system2("/usr/local/bin/twurl",
                   paste0("-H upload.twitter.com \"/1.1/media/upload.json\" -f ",
                          fn,
-                         " -F media -X POST"))
+                         " -F media -X POST"), stdout=TRUE)
 fuzzy_dog <- sub(".*\"media_id\":(\\d+).*", "\\1", fuzzy_dog)
 cat(fuzzy_dog,"\n")
 fuzzy_dog2 <- system2("/usr/local/bin/twurl",
