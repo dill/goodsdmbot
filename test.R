54a82681470fec1a3cf9e7179647c93a2000255c
David Lawrence Miller <github@ninepointeightone.net>
2018-08-14 19:24:20 +0100

    ...

--- test.R
+++ test.R
@@ -1,20 +1,20 @@
 ###
 
+library(ggplot2)
+library(mapdata)
 library(viridis)
 #set.seed(10)
 library(sdmpredictors)
 options(sdmpredictors_datadir="sdmdat")
+source("load_layers_offline.R")
 datasets <- list_datasets(terrestrial = TRUE, marine =FALSE)
 
 layers <- list_layers(datasets)
 
-layercodes <- layers$layer_code[c(1, 2, 21, 33, 45, 57, 69)]#, 89,
-#                                  90, 92, 96, 267)]
- #sample(layers$layer_code, 4)
+layercodes <- layers$layer_code[1:68]
 
 
-
-env <- load_layers(layercodes, equalarea = FALSE, datadir="sdmdat")
+env <- load_layers(layercodes, equalarea = FALSE)
 usa1 <- raster::crop(env, extent(-150, -60, 20, 55))
 
 usa1 <- aggregate(usa1, fact=5)
@@ -26,24 +26,39 @@ dat <- dat[!is.na(dat[,3]),]
 xy <- dat[, 1:2]
 dat <- dat[, -(1:2)]
 
-library(ggplot2)
 
-#dat$p <- as.matrix(dat) %*% matrix(rbinom(ncol(dat),1,0.4)*runif(ncol(dat), -1, 1), ncol=1)
-dat$p <- as.matrix(dat) %*% matrix(rbinom(ncol(dat),1,0.4)*rnorm(ncol(dat)), ncol=1)
-dat$p <- dat$p/max(dat$p)
-dat$p <- dat$p - min(dat$p)
+dat <- apply(dat, 2, function(x) (x-mean(x))/sd(x))
+dat <- as.data.frame(dat)
 
-dat$x <- xy[,1]
-dat$y <- xy[,2]
-dat <- dat[dat$p>0.1, ]
+xlim <- c(-133.54167, -60.20833)
+ylim <- c(20.20833, 54.79167)
+
+dat <- cbind(xy, dat)
+
+generate_map <- function(dat, xy){
+  datr <- as.matrix(dat) %*% 
+            matrix(sample(rep(c(0,1),c(ncol(dat)-4,4)), ncol(dat))*
+              runif(ncol(dat), -1, 1), ncol=1)
+  dat$p <- datr[,1]
+  dat$p <- dat$p - mean(dat$p)
+  dat$p <- dat$p/max(dat$p)
+
+  dat$p <- dat$p*sample(c(-1,1), 1)
+
+  #dat$x <- xy[,1]
+  #dat$y <- xy[,2]
+  dat <- dat[dat$p>0, ]
+
+  p <- ggplot(dat) +
+    geom_tile(aes(x=x,y=y, fill=p)) +
+    scale_fill_viridis() +
+    borders("world", xlim = c(-130, -60), ylim = c(20, 50), colour="black")+
+    theme_void() +
+    coord_equal(xlim=xlim, ylim=ylim)
+
+  return(p)
+}
 
 
-p <- ggplot(dat) +
-  geom_tile(aes(x=x,y=y, fill=p)) +
-  scale_fill_viridis() +
-  coord_equal()
-
-#print(p)
-
-ggsave(p, file="tmp.png", width=800, units="px")
+generate_map(dat, xy)
 
