3b680d49f3e7ac935394378a7742790b471f0e04
David Lawrence Miller <github@ninepointeightone.net>
2018-08-15 19:56:30 +0100

    refactor and cleanup

--- data_setup.R
+++ data_setup.R
@@ -35,30 +35,5 @@ ylim <- c(20.20833, 54.79167)
 
 dat <- cbind(xy, dat)
 
-generate_map <- function(dat, xy){
-  datr <- as.matrix(dat) %*% 
-            matrix(sample(rep(c(0,1),c(ncol(dat)-4,4)), ncol(dat))*
-              runif(ncol(dat), -1, 1), ncol=1)
-  dat$p <- datr[,1]
-  dat$p <- dat$p - mean(dat$p)
-  dat$p <- dat$p/max(dat$p)
 
-  dat$p <- dat$p*sample(c(-1,1), 1)
-
-  #dat$x <- xy[,1]
-  #dat$y <- xy[,2]
-  dat <- dat[dat$p>0, ]
-
-  p <- ggplot(dat) +
-    geom_tile(aes(x=x,y=y, fill=p)) +
-    scale_fill_viridis() +
-    borders("world", xlim = c(-130, -60), ylim = c(20, 50), colour="black")+
-    theme_void() +
-    coord_equal(xlim=xlim, ylim=ylim)
-
-  return(p)
-}
-
-
-generate_map(dat, xy)
-
+save(dat, xy, xlim, ylim, file="saved.RData")
--- thingo.R
+++ thingo.R
@@ -8,8 +8,8 @@ load("saved.RData")
 load("sp_names.RData")
 
 generate_map <- function(dat, xy){
-  datr <- as.matrix(dat) %*% 
-            matrix(sample(rep(c(0,1),c(ncol(dat)-4,4)), ncol(dat))*
+  datr <- as.matrix(dat) %*%
+            matrix(sample(rep(c(0, 1), c(ncol(dat)-4, 4)), ncol(dat))*
               runif(ncol(dat), -1, 1), ncol=1)
   dat$p <- datr[,1]
   dat$p <- dat$p - mean(dat$p)
@@ -26,7 +26,7 @@ generate_map <- function(dat, xy){
     scale_fill_viridis() +
     borders("world", xlim = c(-130, -60), ylim = c(20, 50), colour="black")+
     theme_void() +
-    geom_text(aes(x=-129, y=21), hjust="left",
+    geom_text(x=-132, y=21, hjust="left",
               label=paste(sample(starts, 1), sample(ends, 1))) +
     coord_equal(xlim=xlim, ylim=ylim)
 
@@ -35,16 +35,26 @@ generate_map <- function(dat, xy){
   return(fn)
 }
 
-
-fn <- generate_map(dat, xy)
-
-fuzzy_dog <- system2("/usr/local/bin/twurl",
+post_it <- function(fn){
+  fuzzy_dog <- system2("/usr/local/bin/twurl",
                   paste0("-H upload.twitter.com \"/1.1/media/upload.json\" -f ",
                          fn,
                          " -F media -X POST"), stdout=TRUE)
-fuzzy_dog <- sub(".*\"media_id\":(\\d+).*", "\\1", fuzzy_dog)
-cat(fuzzy_dog,"\n")
-fuzzy_dog2 <- system2("/usr/local/bin/twurl",
-                      paste0("\"/1.1/statuses/update.json\" -d \"media_ids=",
-                             fuzzy_dog,
-                             "&status=\""))
+
+  fuzzy_dog <- sub(".*\"media_id\":(\\d+).*", "\\1", fuzzy_dog)
+  cat(fuzzy_dog,"\n")
+  fuzzy_dog2 <- system2("/usr/local/bin/twurl",
+                        paste0("\"/1.1/statuses/update.json\" -d \"media_ids=",
+                               fuzzy_dog,
+                               "&status=\""))
+}
+
+
+
+fn <- generate_map(dat, xy)
+
+#post_it(fn)
+
+
+
+
