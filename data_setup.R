ea6ae89db917e6065d57204b909d21928536e579
David Lawrence Miller <github@ninepointeightone.net>
2018-08-19 17:27:59 +0100

    new places to get wrong!!

--- data_setup.R
+++ data_setup.R
@@ -12,18 +12,23 @@ options(sdmpredictors_datadir="sdmdat")
 
 # grab some layers
 env <- list()
-inds <- list(1:45, 1:68)
-for(marine in c(TRUE, FALSE)){
-  datasets <- list_datasets(terrestrial=!marine, marine=marine)
-  layers <- list_layers(datasets)
-  layercodes <- layers$layer_code[inds[[marine+1]]]
-  env[[marine+1]] <- load_layers(layercodes, equalarea = FALSE)
-}
+
+# get terrestrial env
+datasets <- list_datasets(terrestrial=TRUE, marine=FALSE)
+layers <- list_layers(datasets)
+layercodes <- layers$layer_code[1:68]
+env[["terrestrial"]] <- load_layers(layercodes, equalarea = FALSE)
+# get marine env
+datasets <- list_datasets(terrestrial=FALSE, marine=TRUE)
+layers <- list_layers(datasets)
+layercodes <- layers$layer_code[1:26]
+env[["marine"]] <- load_layers(layercodes, equalarea = FALSE)
+
 
 get_dat <- function(spec, env){
 
   e <- spec$extent
-  this_env <- env[[spec$marine+1]]
+  this_env <- env[[spec$type]]
   env_crop <- raster::crop(this_env, extent(e[1], e[2], e[3], e[4]))
 
   env_crop <- aggregate(env_crop, fact=5)
@@ -41,9 +46,7 @@ get_dat <- function(spec, env){
 
   dat <- cbind(xy, dat)
 
-  return(list(data=dat,
-              xlim=e[1:2],
-              ylim=e[3:4]))
+  return(dat)
 }
 
 
@@ -51,19 +54,91 @@ get_dat <- function(spec, env){
 # make a list of regions
 spec <- list()
 
-spec[[1]] <- list()
-spec[[1]]$extent <- c(-150, -60, 20, 55)
-spec[[1]]$marine <- FALSE
+# each element of spec is a list with the following elements
+#  extent        - the bounding box for the area
+#  type          - environment type (marine or terrestrial)
+#  dat           - the data.frame of environmental data within extent
+#  pos           - where to position the species name on the plot
 
+# west coast US
+spec[[1]] <- list()
+spec[[1]]$extent <- c(-130, -105, 15, 50)
+spec[[1]]$type <- "marine"
+spec[[1]]$dat <- get_dat(spec[[1]], env)
+spec[[1]]$pos <- "bottomleft"
+
+# North America
 spec[[2]] <- list()
 spec[[2]]$extent <- c(-133.54167, -60.20833, 20.20833, 54.79167)
-spec[[2]]$marine <- FALSE
+spec[[2]]$type <- "terrestrial"
+spec[[2]]$dat <- get_dat(spec[[2]], env)
+spec[[2]]$pos <- "bottomleft"
 
-dd <- get_dat(spec[[1]], env)
+# east coast US w/ GoM
+spec[[3]] <- list()
+spec[[3]]$extent <- c(-100, -70, 15, 45)
+spec[[3]]$type <- "marine"
+spec[[3]]$dat <- get_dat(spec[[3]], env)
+spec[[3]]$pos <- "topleft"
+
+# Australia
+# this doesn't work because of some raster bullshit?
+#spec[[XX]] <- list()
+#spec[[XX]]$extent <- c(110, 155, -XX5, -7)
+#spec[[XX]]$type <- "terrestrial"
+#spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+#spec[[XX]]$pos <- "topleft"
+
+XX <- 4
+# coastal Australia
+spec[[XX]] <- list()
+spec[[XX]]$extent <- c(110, 155, -45, -7)
+spec[[XX]]$type <- "marine"
+spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+spec[[XX]]$pos <- "bottomleft"
+XX <- XX+1
+
+# Africa
+# this doesn't work because of some raster bullshit?
+#spec[[XX]] <- list()
+#spec[[XX]]$extent <- c(-20, 60, -40, 40)
+#spec[[XX]]$type <- "terrestrial"
+#spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+#spec[[XX]]$pos <- "bottomleft"
+#XX <- XX+1
+
+# coastal Africa
+spec[[XX]] <- list()
+spec[[XX]]$extent <- c(-20, 60, -40, 40)
+spec[[XX]]$type <- "marine"
+spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+spec[[XX]]$pos <- "bottomleft"
+XX <- XX+1
+
+# Euro waters
+spec[[XX]] <- list()
+spec[[XX]]$extent <- c(-30, 45, 30, 65)
+spec[[XX]]$type <- "marine"
+spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+spec[[XX]]$pos <- "bottomleft"
+XX <- XX+1
+
+# Euro land
+spec[[XX]] <- list()
+spec[[XX]]$extent <- c(-30, 45, 30, 65)
+spec[[XX]]$type <- "terrestrial"
+spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+spec[[XX]]$pos <- "bottomleft"
+XX <- XX+1
+
+# Indian ocean
+spec[[XX]] <- list()
+spec[[XX]]$extent <- c(45, 110, -30, 23)
+spec[[XX]]$type <- "marine"
+spec[[XX]]$dat <- get_dat(spec[[XX]], env)
+spec[[XX]]$pos <- "bottomleft"
+XX <- XX+1
 
 
-dat <- dd$dat
-xlim <- dd$xlim
-ylim <- dd$ylim
 
-save(dat, xlim, ylim, file="saved.RData")
+save(spec, file="saved.RData")
