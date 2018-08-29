ae7daf1f669c1c50476ca4acd0c6dffed3572051
David Lawrence Miller <github@ninepointeightone.net>
2018-08-29 21:21:54 +0100

    better generation of sp names

--- sp_names.R
+++ sp_names.R
@@ -3,28 +3,39 @@
 type <- c("marine", "terrestrial")
 
 library(stringr)
+library(plyr)
 
 sp_name_dat <- list()
+probs <- list()
 
 for(ty in type){
   files <- dir(paste0("species_names/", ty), full.names=TRUE)
 
-  sp_name_dat[[ty]] <- list()
-  sp_name_dat[[ty]]$starts <- c()
-  sp_name_dat[[ty]]$ends <- c()
+  spp_df <- data.frame(name=NA, order=NA)
+  probs[[ty]] <- c()
 
   for(i in seq_along(files)){
 
     spp <- readLines(files[i])
-    spp_split <- str_split(spp, "\\s+(?=\\S*+$)")
 
-    spp_split <- lapply(spp_split, function(x) if(length(x)==1) c(x, "") else x)
+    # split up the names
+    spp_split <- str_split(spp, "\\s")
+    # empirically, what were the lengths
+    probs[[ty]] <- c(probs[[ty]], laply(spp_split, length))
 
-    spp_final <- t(simplify2array(spp_split))
+    lens <- laply(spp_split, length)
+    lens <- unlist(lapply(lens, function(x) x:1))
 
-    sp_name_dat[[ty]]$starts <- c(sp_name_dat[[ty]]$starts, spp_final[,1])
-    sp_name_dat[[ty]]$ends <- c(sp_name_dat[[ty]]$ends, spp_final[,2])
+    # attach the length, as the names and stack
+    spp_df <- rbind(spp_df,
+                    data.frame(name=unlist(spp_split),
+                               order=lens))
+
   }
+  spp_df <- spp_df[-1, ]
+  sp_name_dat[[ty]] <- dlply(spp_df, .(order))
+  probs[[ty]] <- table(probs[[ty]])
+  probs[[ty]] <- probs[[ty]]/sum(probs[[ty]])
 }
 
-save(sp_name_dat, file="sp_names.RData")
+save(sp_name_dat, probs, file="sp_names.RData")
